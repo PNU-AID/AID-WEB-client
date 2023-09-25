29317b8e5abc3b57f36be9cf366c625a863ec3de
yeonddori <lsymiso@naver.com>
2023-09-26 02:56:02 +0900

    Design: add login/signup layout App.js css -> 각 파일 css로 이동

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -28,35 +28,38 @@ function Login() {
       .catch((e) => console.error('Error: ', e));
   }
   return (
-    <div className="p-8 text-center bg-white rounded-lg shadow-md w-96">
-      <form className="flex flex-col items-center gap-5 w-300">
-        <label className="text-center">AID 로그인</label>
-        <input
-          className="px-4 py-2 border border-gray-300 rounded-lg"
-          id="email"
-          placeholder="Email"
-          ref={emailRef}
-          required
-          type="email"
-        />
-        <input
-          className="px-4 py-2 border border-gray-300 rounded-lg"
-          id="password"
-          placeholder="Password"
-          ref={passwordRef}
-          required
-          type="password"
-        />
-        <button
-          className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-          onClick={handleLogin}
-        >
-          로그인
-        </button>
-      </form>
-      <a className="text-blue-500 underline" href="/signup">
-        signup
-      </a>
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-10 bg-gray-100">
+      <h1 className="text-3xl font-bold">AID</h1>
+      <div className="p-8 text-center bg-white rounded-lg shadow-md w-96">
+        <form className="flex flex-col items-center gap-5 w-300">
+          <label className="text-center">AID 로그인</label>
+          <input
+            className="px-4 py-2 border border-gray-300 rounded-lg"
+            id="email"
+            placeholder="Email"
+            ref={emailRef}
+            required
+            type="email"
+          />
+          <input
+            className="px-4 py-2 border border-gray-300 rounded-lg"
+            id="password"
+            placeholder="Password"
+            ref={passwordRef}
+            required
+            type="password"
+          />
+          <button
+            className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+            onClick={handleLogin}
+          >
+            로그인
+          </button>
+        </form>
+        <a className="text-blue-500 underline" href="/signup">
+          signup
+        </a>
+      </div>
     </div>
   );
 }
