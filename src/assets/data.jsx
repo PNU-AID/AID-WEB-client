19c5389a9d0ba7c4651d853d3f4ea2b24c0394e6
yeonddori <lsymiso@naver.com>
2023-10-08 01:33:55 +0900

    Feat: Study

--- src/assets/data.jsx
+++ src/assets/data.jsx
@@ -7,7 +7,7 @@ import study2BgImg from '../assets/images/curriculums/study2_bg.png';
 import study3BgImg from '../assets/images/curriculums/study3_bg.png';
 import study4BgImg from '../assets/images/curriculums/study4_bg.png';
 
-export const data = {
+export const homeData = {
   title: 'Curriculum',
   subtitle:
     '다양한 스터디와 프로젝트를 운영하고 있으며, 기존에 없는 스터디 및 프로젝트도 직접 생성하고 모집할 수 있습니다.',
@@ -46,3 +46,112 @@ export const data = {
     },
   ],
 };
+
+export const studyData = [
+  {
+    id: 1,
+    name: 'AID_WEB(frontend)',
+    info: '스터디장: aaa',
+    date: '모집기간: 10.01 - 10.10',
+    category: '',
+    status: '모집 중',
+    image: study1Img,
+  },
+  {
+    id: 2,
+    name: 'AID_WEB(backend)',
+    info: '스터디장: aaa',
+    date: '모집기간: 10.01 - 10.10',
+    category: '',
+    status: '모집 중',
+    image: study1Img,
+  },
+  {
+    id: 3,
+    name: '논문스터디',
+    info: '스터디장: aaa',
+    date: '모집기간: 10.01 - 10.10',
+    category: '논문',
+    status: '진행 중',
+    image: study1Img,
+  },
+  {
+    id: 4,
+    name: '자연어',
+    info: '스터디장: aaa',
+    date: '모집기간: 10.01 - 10.10',
+    category: '자연어',
+    status: '진행 완료',
+    image: study1Img,
+  },
+  {
+    id: 5,
+    name: '혼자 공부하는 머신러닝',
+    info: '스터디장: aaa',
+    date: '모집기간: 10.01 - 10.10',
+    category: '머신러닝',
+    status: '진행 완료',
+    image: study1Img,
+  },
+  {
+    id: 6,
+    name: '혼자 공부하는 머신러닝',
+    info: '스터디장: aaa',
+    date: '모집기간: 10.01 - 10.10',
+    category: '머신러닝',
+    status: '진행 완료',
+    image: study1Img,
+  },
+  {
+    id: 7,
+    name: '혼자 공부하는 머신러닝',
+    info: '스터디장: aaa',
+    date: '모집기간: 10.01 - 10.10',
+    category: '머신러닝',
+    status: '진행 완료',
+    image: study1Img,
+  },
+  {
+    id: 8,
+    name: '혼자 공부하는 머신러닝',
+    info: '스터디장: aaa',
+    date: '모집기간: 10.01 - 10.10',
+    category: '머신러닝',
+    status: '진행 완료',
+    image: study1Img,
+  },
+];
+
+export const categoryData = [
+  {
+    id: 1,
+    name: 'All',
+  },
+  {
+    id: 2,
+    name: '논문',
+  },
+  {
+    id: 3,
+    name: '자연어',
+  },
+  {
+    id: 4,
+    name: '머신러닝',
+  },
+];
+
+export const statusData = [
+  {
+    id: 1,
+    name: '모집 중',
+  },
+  {
+    id: 2,
+    name: '진행 중',
+  },
+  {
+    id: 3,
+    name: '진행 완료',
+  },
+];
